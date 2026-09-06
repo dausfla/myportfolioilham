4990b62d3b7c4ed7c1ec245521e057de583f3ea9
Ilham Ramadhan Setiawan <ilhamramadhan@portfolio.com>
2026-09-07 00:50:30 +0700

    Remove Vercel files for native PHP hosting migration

--- api/contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('BASE_DIR', dirname(__DIR__));
-try {
-    require_once BASE_DIR . '/contact.php';
-} catch (\Throwable $e) {
-    http_response_code(500);
-    echo '<h1>Error</h1><pre>' . htmlspecialchars($e->getMessage()) . '</pre>';
-}
